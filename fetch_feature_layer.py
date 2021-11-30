be86f17e8eccf33581311582d8169f498440b2eb
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-11-30 11:56:04 -0800

    create table, insert, update

--- fetch_feature_layer.py
+++ fetch_feature_layer.py
@@ -7,11 +7,49 @@
 
 import urllib.parse
 import urllib.request
-import os
 import json
-import tempfile
+from datetime import datetime
 import fire
-import subprocess
+from sqlalchemy import Table, MetaData, Column, Integer, Float, Text, TIMESTAMP, create_engine
+from sqlalchemy.sql import select
+from geoalchemy2.types import Geometry
+from shapely.geometry import shape
+from shapely.geometry.multipolygon import MultiPolygon
+from shapely.geometry.polygon import Polygon
+from shapely import wkb
+
+
+def get_column_type(value):
+    """ Return the correct sqlalchemy column type for a given value. """
+    if isinstance(value, int):
+        return Integer()
+    elif isinstance(value, float):
+        return Float()
+    elif isinstance(value, str):
+        return Text()
+    raise Exception(f'unknown type {type(value)}')
+
+def create_table_schema(meta_data: MetaData, data: dict, table_name: str, geom_type: str, srid: int) -> Table:
+    """
+    Create a table schema.
+
+    geom_type: geometry type (e.g. POLYGON or MULTIPOLYGON)
+    srid: spatial reference id (e.g. 4326)
+    """
+    columns = {}
+    for feature in data['features']:
+        for key, value in feature['properties'].items():
+            if not key in columns.keys():
+                columns[key] = Column(key.lower(), get_column_type(value))  
+                # print(f'{key}:{value} {type(value)}')
+
+    return Table(table_name, meta_data,
+                 Column('id', Integer(), primary_key=True, nullable=False),
+                 Column('geom', Geometry(geometry_type=geom_type, srid=srid, spatial_index=True, from_text='ST_GeomFromEWKT', name='geometry'), nullable=False),
+                 Column('create_date', TIMESTAMP(timezone=True), nullable=False),
+                 Column('update_date', TIMESTAMP(timezone=True), nullable=False),
+                 *list(columns.values()),
+                 schema=None)
 
 
 def fetch_object_list(url: str):
@@ -67,7 +105,7 @@ def fetch_object(object_id: int, url: str):
     json_data = json.loads(response.read())
     return json_data
 
-def sync_layer(url: str, host: str, dbname: str, user: str, password: str, table: str):
+def sync_layer(url: str, host: str, dbname: str, user: str, password: str, table: str, geom_type: str = 'MULTIPOLYGON', srid: int = 4326, port: int = 5432):
     """
     Sync a feature layer.
 
@@ -77,24 +115,55 @@ def sync_layer(url: str, host: str, dbname: str, user: str, password: str, table
     user: database user (e.g. tileserver)
     password: database password (e.g. tileserver)
     table: table name (e.g. my_fancy_table)
+    geom_type: e.g. POLYGON or MULTIPOLYGON
+    srid: e.g. 4326
     """
     print(f'syncing {url}...')
 
-    ids = fetch_object_list(url)
-    for id in ids:
-        obj = fetch_object(id, url)
-        with tempfile.TemporaryDirectory() as temporary_path:
-            filename = os.path.join(os.getcwd(), temporary_path, f'obj_{id}.json')
-            with open(filename, "w") as f:
-                json.dump(obj, f)
-            # NOTE: There's an issue with 453 (that's the Fraser Fire Zone) - it's being served up as a multipolygon which happens
-            # to also contain Haida Gwaii. All other features are polygons, so the table as greated with a polygon geom column. When
-            # this feature is encountered, it fails because it can't squeeze a multipolygon into a polygon.
-            command = f'ogr2ogr -f "PostgreSQL" PG:"dbname={dbname} host={host} user={user} password={password}" "{filename}" -nln {table}'
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-            process.wait()
-            print(f'process exited with code {process.returncode}')
-            os.remove(filename)
+    meta_data = MetaData()
+    DB_STRING = f'postgresql://{user}:{password}@{host}:{port}/{dbname}'
+    engine = create_engine(DB_STRING, connect_args={'options': '-c timezone=utc'})
+    table_schema = None
+
+    with engine.connect() as connection:
+
+        ids = fetch_object_list(url)
+        for id in ids:
+            obj = fetch_object(id, url)
+
+            if table_schema is None:
+                # We base our table off the first object we get back.
+                table_schema = create_table_schema(meta_data, obj, table, geom_type, srid)
+
+            if not engine.dialect.has_table(connection, table):
+                meta_data.create_all(engine)
+
+            for feature in obj['features']:
+                geom = shape(feature['geometry'])
+
+                # If we're expecting multipolygons, we need to convert the polygon to a multipolygon.
+                if geom_type == 'MULTIPOLYGON' and isinstance(geom, Polygon):
+                    geom = MultiPolygon([geom])
+
+                wkt = wkb.dumps(geom, hex=True, srid=srid)
+                props = {key.lower(): value for key, value in feature['properties'].items()}
+                values = {'id': feature['id'], 'geom': wkt, **props}
+
+                rows = connection.execute(select(table_schema).where(table_schema.c.id == feature['id']))
+                exists = False
+                for row in rows:
+                    if row:
+                        exists = True
+                    break
+
+                values['update_date'] = datetime.now()
+                if exists:
+                    print('record exists, updating')
+                    connection.execute(table_schema.update().where(table_schema.c.id == feature['id']).values(values))
+                else:
+                    print('create new record')
+                    values['create_date'] = values['update_date']
+                    connection.execute(table_schema.insert().values(values))
 
 if __name__ == '__main__':
     fire.Fire(sync_layer)
